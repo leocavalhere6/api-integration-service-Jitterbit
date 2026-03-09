b3856f6dd1aa447fb658cc6664296eda8a9aa693
leonardo Henrique De Oliveira Cavalhere <leocavalhere6@gmail.com>
2026-03-09 08:05:20 +0000

    test: add initial api health check test

---
+++ test/app.test.js
@@ -0,0 +1,11 @@
+const request = require("supertest");
+const app = require("../src/app");
+
+describe("API Health Check", () => {
+  it("should return 200 when calling root endpoint", async () => {
+    const response = await request(app).get("/");
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.message).toBe("Order API running");
+  });
+});
